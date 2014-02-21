cd44f3e84167bc4819e4b32eb76500220290c99d
Elvis Stansvik <elvstone@gmail.com>
2014-02-21 02:46:02 +0100

    Add some command line parsing.
    usage: java ARMMain <file> <options>
     -g         print abstract syntax tree (GraphViz)
     -h         show help message
     -o <arg>   output file
     -p         print abstract syntax tree
     -S         output assembler code

--- src/main/java/mjc/ARMMain.java
+++ src/main/java/mjc/ARMMain.java
@@ -3,41 +3,73 @@ package mjc;
 import java.io.PushbackReader;
 import java.io.FileReader;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import mjc.lexer.Lexer;
 import mjc.parser.Parser;
 import mjc.node.Start;
+import mjc.analysis.ASTGraphPrinter;
 import mjc.analysis.ASTPrinter;
 import mjc.analysis.BasicAnalyzer;
 
 public class ARMMain {
     private BasicAnalyzer analyzer = new BasicAnalyzer();
-    private ASTPrinter printer = new ASTPrinter();
+    private ASTPrinter astPrinter = new ASTPrinter();
+    private ASTGraphPrinter graphPrinter = new ASTGraphPrinter();
 
     private void run(String[] args) {
+        Options options = new Options();
+        options.addOption("S", false, "output assembler code");
+        options.addOption("o", true, "output file");
+        options.addOption("h", false, "show help message");
+        options.addOption("p", false, "print abstract syntax tree");
+        options.addOption("g", false, "print abstract syntax tree (GraphViz)");
+
+        CommandLineParser commandLineParser = new GnuParser();
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter.setOptionComparator(null);
+
         try {
+            CommandLine commandLine = commandLineParser.parse(options, args);
+
+            if (commandLine.hasOption("h") || commandLine.getArgs().length != 1) {
+                helpFormatter.printHelp("java ARMMain <file> <options>", options);
+                System.exit(1);
+            }
+
             // Parse input from the specified file.
-            PushbackReader in = new PushbackReader(new FileReader(args[0]));
+            PushbackReader in = new PushbackReader(new FileReader(commandLine.getArgs()[0]));
             Parser parser = new Parser(new Lexer(in));
             Start tree = parser.parse();
 
             // Run basic analysis.
-            if (!analyzer.analyze(tree))
-                return;
+            if (!analyzer.analyze(tree)) {
+                System.exit(1);
+            }
 
             // Print AST to log.
-            printer.print(tree);
+            if (commandLine.hasOption("p")) {
+                astPrinter.print(tree);
+            }
 
+            // Print AST in GraphViz format.
+            if (commandLine.hasOption("g")) {
+                graphPrinter.print(tree);
+            }
+
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
         } catch (Exception e) {
-            System.out.println(e);
+            System.err.println(e);
         }
     }
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java mjc.ARMMain <file>");
-            System.exit(1);
-        }
-
         ARMMain program = new ARMMain();
         program.run(args);
     }
