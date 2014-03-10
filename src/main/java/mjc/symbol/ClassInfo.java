e83b93e7ce6b90398886b4b2f93d0ae9840a0444
Elvis Stansvik <elvstone@gmail.com>
2014-03-10 17:04:42 +0100

    Documentation for ClassInfo.

--- src/main/java/mjc/symbol/ClassInfo.java
+++ src/main/java/mjc/symbol/ClassInfo.java
@@ -6,6 +6,9 @@ import java.util.Map;
 
 import mjc.types.ClassType;
 
+/**
+ * ClassInfo represents information about a declared class.
+ */
 public class ClassInfo {
 
     private final String name;
@@ -16,6 +19,14 @@ public class ClassInfo {
     private final int line;
     private final int column;
 
+    /**
+     * Construct a new ClassInfo.
+     *
+     * @param name Name of the class.
+     * @param type Type of the class.
+     * @param line Line of declaration.
+     * @param column Column of declaration.
+     */
     public ClassInfo(String name, ClassType type, int line, int column) {
         this.name = name;
         this.type = type;
@@ -26,42 +37,89 @@ public class ClassInfo {
         methods = new HashMap<>();
     }
 
+    /**
+     * @return Name of the class.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return Type of the class.
+     */
     public ClassType getType() {
         return type;
     }
 
+    /**
+     * @return Line of declaration.
+     */
     public int getLine() {
         return line;
     }
 
+    /**
+     * @return Column of declaration.
+     */
     public int getColumn() {
         return column;
     }
 
+    /**
+     * Returns information about a field of the class.
+     *
+     * @param name Name of the field.
+     * @return Information about the field, or null if the class has no such field.
+     */
     public VariableInfo getField(String name) {
         return fields.get(name);
     }
 
+    /**
+     * @return Collection of the fields of the class.
+     */
     public Collection<VariableInfo> getFields() {
         return fields.values();
     }
 
+    /**
+     * Adds information about a field of the class.
+     *
+     * If there's already information about a field with the same name, it will be
+     * replaced.
+     *
+     * @param field Information about the field.
+     */
     public void addField(VariableInfo field) {
         fields.put(field.getName(), field);
     }
 
+    /**
+     * Returns information about a method of the class.
+     *
+     * @param name Name of the method.
+     * @return Information about the method, or null if the class has no such method.
+     */
     public MethodInfo getMethod(String name) {
         return methods.get(name);
     }
 
+    /**
+     * @return Collection of the methods of the class.
+     */
     public Collection<MethodInfo> getMethods() {
         return methods.values();
     }
 
+    /**
+     * Adds information about a method of the class.
+     *
+     * If there's already information about a method with the same name, it will be
+     * replaced.
+     *
+     * @param name Name of the method.
+     * @param method Information about the method.
+     */
     public void addMethod(String name, MethodInfo info) {
         methods.put(name, info);
     }
