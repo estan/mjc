5c688eafcf9acd73d331e975b743278a0700d404
Elvis Stansvik <elvstone@gmail.com>
2014-03-16 16:07:31 +0100

    Add type-check test for int literal.

---
+++ src/test/resources/type_error/IntegerExpression.java
@@ -0,0 +1,7 @@
+class IntegerExpressionTest {
+    public static void main(String[] args) {
+        int i;
+        //i = 2147483647; // OK!
+        i = 2147483648; // INVALID_INT_LITERAL
+    }
+}
