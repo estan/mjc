a694877701e046155be48ef4857ac996a301833e
Elvis Stansvik <elvstone@gmail.com>
2014-03-16 15:08:32 +0100

    Add comment about allowed accesses.

--- src/test/resources/type_error/ArrayAccessExpression.java
+++ src/test/resources/type_error/ArrayAccessExpression.java
@@ -8,12 +8,14 @@ class ArrayAccessExpressionTest {
         A cA;
         B cB;
 
+        //i = ia[i];// OK!
         i = ia[ia]; // INVALID_INDEX_TYPE
         i = ia[l];  // INVALID_INDEX_TYPE
         i = ia[la]; // INVALID_INDEX_TYPE
         i = ia[b];  // INVALID_INDEX_TYPE
         i = ia[cA]; // INVALID_INDEX_TYPE
 
+        //l = la[i];  // OK!
         l = la[ia];  // INVALID_INDEX_TYPE
         l = la[l];   // INVALID_INDEX_TYPE
         l = la[la];  // INVALID_INDEX_TYPE
