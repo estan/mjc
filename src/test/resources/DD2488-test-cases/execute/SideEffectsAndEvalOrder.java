0cd93e851d8eba87eb0cb8fe490d0444062740bc
Elvis Stansvik <elvstone@gmail.com>
2014-03-16 19:54:57 +0100

    Improve test case.
    Mix it up with some + and -.

--- src/test/resources/DD2488-test-cases/execute/SideEffectsAndEvalOrder.java
+++ src/test/resources/DD2488-test-cases/execute/SideEffectsAndEvalOrder.java
@@ -2,7 +2,7 @@ class SideEffectsAndEvalOrder {
     public static void main(String[] args) {
         IncredibleMachine m;
         m = new IncredibleMachine();
-        System.out.println(m.run());
+        System.out.println(m.run()); // Should print 329.
     }
 }
 
@@ -10,15 +10,23 @@ class IncredibleMachine {
     int result;
     public int run() {
         result = 2;
-        if (a(3, false) && a(5, true)                        /* r = 2 * 3 */
-            || (a(7, false) || a(11, true) && a(13, false))  /* r = 2 * 3 * 7 * 11 * 13 */
-            || a(17, true) || a(19, true))                   /* r = 2 * 3 * 7 * 11 * 13 * 17 = 102102 */
+        if (a(3, false) && a(5, true)                        /* r = 2 + 3 */
+            || (m(7, false) || s(11, true) && m(13, false))  /* r = ((2 + 3) * 7 - 11) * 13 */
+            || a(17, true) || m(19, true))                   /* r = ((2 + 3) * 7 - 11) * 13 + 17 = 329 */
             result = result;
         else {}
         return result;
     }
-    public boolean a(int value, boolean ret) {
+    public boolean m(int value, boolean ret) {
         result = result * value;
+        return ret;
+    }
+    public boolean a(int value, boolean ret) {
+        result = result + value;
+        return ret;
+    }
+    public boolean s(int value, boolean ret) {
+        result = result - value;
         return ret;
     }
 }
