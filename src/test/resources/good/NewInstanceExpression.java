398be49d21a63acfa906ae81756ab91c03cdb531
Elvis Stansvik <elvstone@gmail.com>
2014-03-17 00:30:47 +0100

    Update test.

--- src/test/resources/good/NewInstanceExpression.java
+++ src/test/resources/good/NewInstanceExpression.java
@@ -1,7 +1,9 @@
 class NewInstanceExpressionTest {
     public static void main(String[] args) {
         A cA;
+        NewInstanceExpression foo;
         cA = new A(); // OK!
+        foo = new NewInstanceExpressionTest(); // OK!
     }
 }
 class A {}
