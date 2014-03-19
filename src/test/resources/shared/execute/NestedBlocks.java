232ef02d173cda4f1cd4ca099de5c07e3f2a1343
Elvis Stansvik <elvstone@gmail.com>
2014-03-19 13:18:36 +0100

    Add execute test for nested blocks.

---
+++ src/test/resources/shared/execute/NestedBlocks.java
@@ -0,0 +1,66 @@
+/**
+ * Testing nested block declarations. Should print the numbers from the
+ * TV series lost:
+ *
+ * 4
+ * 8
+ * 15
+ * 16
+ * 23
+ * 42
+ *
+ * @author Elvis Stansvik
+ */
+
+//EXT:NBD
+
+class NestedBlocks {
+    public static void main(String[] args) {
+        Lost lost;
+        lost = new Lost();
+        lost.printNumbers();
+    }
+}
+
+class Lost {
+    public int printNumbers() {
+        int l, k;
+        {
+            int n;
+            n = 1;
+        }
+        {
+            int n;
+            n = 8;
+            {
+                int m;
+                m = 4;
+                {
+                    System.out.println(m);
+                }
+            }
+            System.out.println(n);
+            {
+                int m;
+                m = 15;
+                {
+                    System.out.println(m);
+                }
+            }
+        }
+        k = 16;
+        {
+            int n;
+            n = 23;
+            {
+                System.out.println(k);
+            }
+            System.out.println(n);
+        }
+        {
+            l = 42;
+        }
+        System.out.println(l);
+        return 0;
+    }
+}
