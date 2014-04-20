3e07d02cb13cb7745849ffc93bfff69014fe7099
Elvis Stansvik <elvstone@gmail.com>
2014-04-20 16:37:37 +0200

    Add another shared noncompile test.

---
+++ src/test/resources/shared/noncompile/MissingReturn.java
@@ -0,0 +1,13 @@
+/**
+ * Check that method without return is not accepted.
+ *
+ * @author Elvis Stansvik <elvstone@gmail.com>
+ */
+class MissingReturn {
+    public static void main(String[] args) {
+    }
+}
+class MissingReturnTest {
+    public int foo() {
+    }
+}
