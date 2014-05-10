ca1a26cf8432e9f4a7a2485e57bc184e656a28be
Elvis Stansvik <elvstone@gmail.com>
2014-05-10 21:55:49 +0200

    Improve test.

--- src/test/resources/compile/PrintlnStatement.java
+++ src/test/resources/compile/PrintlnStatement.java
@@ -1,7 +1,9 @@
 class PrintlnStatementTest {
     public static void main(String[] args) {
         int i;
+        boolean b;
 
         System.out.println(i); // OK!
+        System.out.println(b); // OK!
     }
 }
