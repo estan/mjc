3c134f145ac7f44d0a8c56cd0f7e57bef9f9b077
Elvis Stansvik <elvstone@gmail.com>
2014-05-10 21:54:47 +0200

    Fix test for println.
    Boolean types can be printed.

--- src/test/resources/noncompile/type/PrintlnStatement.java
+++ src/test/resources/noncompile/type/PrintlnStatement.java
@@ -8,7 +8,7 @@ class PrintlnStatementTest {
 
         //System.out.println(i); // OK!
         System.out.println(ia); // INVALID_PRINTLN_TYPE
-        System.out.println(b); // INVALID_PRINTLN_TYPE
+        //System.out.println(b); // OK!
         System.out.println(cA); // INVALID_PRINTLN_TYPE
         System.out.println(cB); // INVALID_PRINTLN_TYPE
     }
