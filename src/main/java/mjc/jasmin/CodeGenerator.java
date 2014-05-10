b17e2717306126091c313e75b7b26301671acd44
Elvis Stansvik <elvstone@gmail.com>
2014-05-10 20:31:48 +0200

    More work on Jasmin generation.

--- src/main/java/mjc/jasmin/CodeGenerator.java
+++ src/main/java/mjc/jasmin/CodeGenerator.java
@@ -6,12 +6,24 @@ import java.util.Map;
 import mjc.analysis.DepthFirstAdapter;
 import mjc.node.ABlockStatement;
 import mjc.node.AClassDeclaration;
+import mjc.node.AFalseExpression;
+import mjc.node.AFieldDeclaration;
+import mjc.node.AIntegerExpression;
 import mjc.node.AMainClassDeclaration;
 import mjc.node.AMethodDeclaration;
+import mjc.node.AMinusExpression;
+import mjc.node.ANotExpression;
+import mjc.node.APlusExpression;
+import mjc.node.APrintlnStatement;
+import mjc.node.AThisExpression;
+import mjc.node.ATimesExpression;
+import mjc.node.ATrueExpression;
+import mjc.node.AVariableDeclaration;
 import mjc.node.Node;
 import mjc.symbol.ClassInfo;
 import mjc.symbol.MethodInfo;
 import mjc.symbol.SymbolTable;
+import mjc.symbol.VariableInfo;
 import mjc.types.Type;
 
 /**
@@ -20,6 +32,8 @@ import mjc.types.Type;
  * TODO: More docs.
  */
 public class CodeGenerator extends DepthFirstAdapter {
+    private final static int MAX_STACK_SIZE = 30; // TODO: Don't hardcode this.
+
     private SymbolTable symbolTable;
     private Map<Node, Type> types;
 
@@ -33,15 +47,73 @@ public class CodeGenerator extends DepthFirstAdapter {
         this.types = types;
     }
 
+    private String typeDescriptor(final Type type) {
+        if (type.isInt()) {
+            return "I";
+        } else if (type.isIntArray()) {
+            return "[I";
+        } else if (type.isBoolean()) {
+            return "B";
+        } else if (type.isClass()) {
+            return "L" + type.getName() + ";";
+        } else {
+            throw new Error("Unknown Type");
+        }
+    }
+
+    private void writeClass() {
+        // Just write to standard output for now.
+        try {
+            out.write(System.out);
+            System.out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        out.clear();
+    }
+
+    private String methodSignatureDescriptor(final MethodInfo methodInfo) {
+        String descriptor = "(";
+        for (VariableInfo paramInfo : methodInfo.getParameters()) {
+            descriptor += typeDescriptor(paramInfo.getType());
+        }
+        descriptor += ")" + typeDescriptor(methodInfo.getReturnType());
+        return descriptor;
+    }
+
     @Override
     public void inAMainClassDeclaration(final AMainClassDeclaration declaration) {
         currentClass = symbolTable.getClassInfo(declaration.getName().getText());
         currentMethod = currentClass.getMethod(declaration.getMethodName().getText());
         currentMethod.enterBlock();
+
+        out.directive("class public %s", currentClass.getName());
+        out.directive("super java/lang/Object");
+        out.newline();
+
+        out.bigcomment("Default constructor");
+        out.directive("method public <init>()V");
+        out.aload(0);
+        out.instruction("invokenonvirtual java/lang/Object/<init>()V");
+        out.instruction("return");
+        out.directive("end method");
+        out.newline();
+
+        final int numLocals = currentMethod.getNumParameters() + currentMethod.getNumLocals() + 1;
+
+        out.bigcomment("Main method");
+        out.directive("method public static main([Ljava/lang/String;)V");
+        out.directive("limit locals %d", numLocals);
+        out.directive("limit stack %d", MAX_STACK_SIZE);
     }
 
     @Override
     public void outAMainClassDeclaration(final AMainClassDeclaration declaration) {
+        out.instruction("return");
+        out.directive("end method");
+
+        writeClass();
+
         currentMethod.leaveBlock();
         currentMethod = null;
         currentClass = null;
@@ -51,20 +123,23 @@ public class CodeGenerator extends DepthFirstAdapter {
     public void inAClassDeclaration(final AClassDeclaration declaration) {
         currentClass = symbolTable.getClassInfo(declaration.getName().getText());
 
-        out.descriptor("class public %s", currentClass.getName());
-        out.descriptor("super java/lang/Object");
+        out.directive("class public %s", currentClass.getName());
+        out.directive("super java/lang/Object");
     }
 
     @Override
     public void outAClassDeclaration(final AClassDeclaration declaration) {
-        // Just write to standard output for now.
-        try {
-            out.write(System.out);
-            System.out.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        out.clear();
+
+        out.newline();
+        out.bigcomment("Default constructor");
+        out.directive("method public <init>()V");
+        out.aload(0);
+        out.instruction("invokenonvirtual java/lang/Object/<init>()V");
+        out.instruction("return");
+        out.directive("end method");
+        out.newline();
+
+        writeClass();
 
         currentClass = null;
     }
@@ -73,12 +148,35 @@ public class CodeGenerator extends DepthFirstAdapter {
     public void inAMethodDeclaration(final AMethodDeclaration declaration) {
         currentMethod = currentClass.getMethod(declaration.getName().getText());
         currentMethod.enterBlock();
+
+        final String methodName = currentMethod.getName();
+        final String methodSignature = methodSignatureDescriptor(currentMethod);
+        final int numLocals = currentMethod.getNumParameters() + currentMethod.getNumLocals() + 1;
+
+        out.newline();
+        out.bigcomment("Method " + methodName);
+        out.directive("method public %s%s", methodName, methodSignature);
+        out.directive("limit locals %d", numLocals);
+        out.directive("limit stack %d", MAX_STACK_SIZE);
     }
 
     @Override
     public void outAMethodDeclaration(final AMethodDeclaration declaration) {
+        final Type type = currentMethod.getReturnType();
+
+        out.instruction(type.isClass() || type.isIntArray() ? "areturn" : "ireturn");
+        out.directive("end method");
+
         currentMethod.leaveBlock();
         currentMethod = null;
+    }
+
+    @Override
+    public void inAFieldDeclaration(final AFieldDeclaration declaration) {
+        final String fieldName = declaration.getName().getText();
+        final Type fieldType = currentClass.getField(fieldName).getType();
+
+        out.directive("field protected %s %s", typeDescriptor(fieldType), fieldName);
     }
 
     @Override
@@ -89,5 +187,61 @@ public class CodeGenerator extends DepthFirstAdapter {
     @Override
     public void outABlockStatement(final ABlockStatement block) {
         currentMethod.leaveBlock();
+    }
+
+    @Override
+    public void inAPrintlnStatement(final APrintlnStatement statement) {
+        out.instruction("getstatic java/lang/System/out Ljava/io/PrintStream;");
+    }
+
+    @Override
+    public void outAPrintlnStatement(final APrintlnStatement statement) {
+        out.instruction("invokestatic java/lang/String/valueOf(I)Ljava/lang/String;");
+        out.instruction("invokevirtual java/io/PrintStream/println(Ljava/lang/String;)V");
+    }
+
+    @Override
+    public void outAPlusExpression(final APlusExpression expression) {
+        out.instruction("iadd");
+    }
+
+    @Override
+    public void outAMinusExpression(final AMinusExpression expression) {
+        out.instruction("isub");
+    }
+
+    @Override
+    public void outATimesExpression(final ATimesExpression expression) {
+        out.instruction("imul");
+    }
+
+    @Override
+    public void outAIntegerExpression(final AIntegerExpression expression) {
+        out.iconst(Integer.valueOf(expression.getInteger().getText()));
+    }
+
+    @Override
+    public void outATrueExpression(final ATrueExpression expression) {
+        out.iconst(1);
+    }
+
+    @Override
+    public void inANotExpression(final ANotExpression expression) {
+        out.iconst(1);
+    }
+
+    @Override
+    public void outANotExpression(final ANotExpression expression) {
+        out.instruction("isub");
+    }
+
+    @Override
+    public void outAFalseExpression(final AFalseExpression expression) {
+        out.iconst(0);
+    }
+
+    @Override
+    public void outAThisExpression(final AThisExpression expression) {
+        out.aload(0);
     }
 }
--- src/main/java/mjc/jasmin/JasminWriter.java
+++ src/main/java/mjc/jasmin/JasminWriter.java
@@ -12,13 +12,13 @@ class JasminWriter {
     private StringBuilder out = new StringBuilder();
 
     /**
-     * Adds a Jasmin descriptor to the writer.
+     * Adds a Jasmin directive to the writer.
      *
-     * @param descriptor Descriptor to add, including any format specifiers.
-     * @param args Arguments matching format specifiers in @a descriptor.
+     * @param directive Directive to add, including any format specifiers.
+     * @param args Arguments matching format specifiers in @a directive.
      */
-    void descriptor(String descriptor, Object... args) {
-        out.append('.' + String.format(descriptor, args) + '\n');
+    void directive(String directive, Object... args) {
+        out.append('.' + String.format(directive, args) + '\n');
     }
 
     /**
@@ -38,6 +38,71 @@ class JasminWriter {
      */
     void label(String label) {
         out.append(label + ":\n");
+    }
+
+    /**
+     * Adds an iconst_# or ldc instruction to the writer.
+     *
+     * @param c Integer constant to push.
+     */
+    void iconst(int c) {
+        instruction(0 <= c && c <= 5 ? "iconst_%d" : "ldc %d", c);
+    }
+
+    /**
+     * Adds an iload_# or iload instruction to the writer.
+     *
+     * @param i Variable index.
+     */
+    void iload(int i) {
+        instruction("iload%s", (0 <= i && i <= 3 ? "_" : " ") + i);
+    }
+
+    /**
+     * Adds an istore_# or istore instruction to the writer.
+     *
+     * @param i Variable index.
+     */
+    void istore(int i) {
+        instruction("istore%s", (0 <= i && i <= 3 ? "_" : " ") + i);
+    }
+
+    /**
+     * Adds an aload_# or aload instruction to the writer.
+     *
+     * @param i Variable index.
+     */
+    void aload(int i) {
+        instruction("aload%s", (0 <= i && i <= 3 ? "_" : " ") + i);
+    }
+
+    /**
+     * Adds a Jasmin comment to the writer.
+     *
+     * @param comment A comment, including any format specifiers.
+     * @param args Arguments matching format specifiers in @a comment.
+     */
+    void comment(String comment, Object... args) {
+        out.append("; " + String.format(comment, args) + '\n');
+    }
+
+    /**
+     * Adds a big Jasmin comment to the writer.
+     *
+     * @param comment A comment, including any format specifiers.
+     * @param args Arguments matching format specifiers in @a comment.
+     */
+    void bigcomment(String comment, Object... args) {
+        comment("");
+        comment(comment, args);
+        comment("");
+    }
+
+    /**
+     * Adds a newline to the writer.
+     */
+    void newline() {
+        out.append("\n");
     }
 
     /**
